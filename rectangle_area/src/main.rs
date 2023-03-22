24e27c6afc5d1f00b9e81115fc8a9dfbcc022502
Rohith Kanchi <rohithk2016@gmail.com>
2023-03-23 00:39:44 +0530

    use methods of a struct.

--- rectangle_area/src/main.rs
+++ rectangle_area/src/main.rs
@@ -1,19 +1,32 @@
 #[derive(Debug)]
-struct Rectangle{
+struct Rectangle {
     length: u32,
-    width: u32
+    width: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.length
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
 }
 
 fn main() {
-    let dimensions = Rectangle{
+    let rect = Rectangle {
         length: 50,
         width: 30,
     };
-    println!("rect: {:#?}", dimensions);
-    println!("Area of rectangle is {} square pixels", calculate_area(&dimensions));
-}
 
-//pass reference to Rectangle as we want to access its fields but not take ownership
-fn calculate_area(dimensions: &Rectangle) -> u32{
-    dimensions.length*dimensions.width
+    let rect2: Rectangle = Rectangle {
+        length: 40,
+        width: 15,
+    };
+    println!("rect: {:#?}", rect);
+    println!("Area of rectangle is {} square pixels", rect.area());
+    println!("Area of rectangle is {} square pixels", rect2.area());
+    println!("Rect can hold Rect2: {}", rect.can_hold(&rect2));
+
 }
