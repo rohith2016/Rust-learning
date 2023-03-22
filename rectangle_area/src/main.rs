0aa0c92b813c6c70d2cbcbacdcdc4a5797e3ef21
Rohith Kanchi <rohithk2016@gmail.com>
2023-03-23 00:27:51 +0530

    use structs

--- rectangle_area/src/main.rs
+++ rectangle_area/src/main.rs
@@ -1,11 +1,19 @@
-fn main() {
-    let width: u32 = 30;
-    let length: u32 = 50;
-    
-    let area = calculate_area1(width, length);
-    println!("Area of rectangle is {} square pixels", area);
+#[derive(Debug)]
+struct Rectangle{
+    length: u32,
+    width: u32
 }
 
-fn calculate_area1(width: u32, length: u32) -> u32{
-    return width*length;
+fn main() {
+    let dimensions = Rectangle{
+        length: 50,
+        width: 30,
+    };
+    println!("rect: {:#?}", dimensions);
+    println!("Area of rectangle is {} square pixels", calculate_area(&dimensions));
+}
+
+//pass reference to Rectangle as we want to access its fields but not take ownership
+fn calculate_area(dimensions: &Rectangle) -> u32{
+    dimensions.length*dimensions.width
 }
